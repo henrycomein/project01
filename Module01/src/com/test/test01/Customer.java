649f4a3ff4af1af45f89a60c2232e66728c1f734
henry <yfluanfeng@qq.com>
2019-07-10 21:51:23 +0800

    Initial commit

---
+++ Module01/src/com/test/test01/Customer.java
@@ -0,0 +1,7 @@
+package com.test.test01;
+
+public class Customer {
+    public static void main(String[] args){
+        System.out.println("cc");
+    }
+}
---
+++ src/com/test/java/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.test.java;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+       String a ="";
+
+    }
+
+    //这是注释
+    public void Method(){
+
+    }
+
+}
